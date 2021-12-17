b9dd8b61bc9f84f75e2fa69f05ae3282ba3e2744
Bingshuai Duanmu <bingshuai.duanmu@phyplusinc.com>
2021-12-17 14:36:10 +0800

    sdk release repo push

--- _bld_script/sdk_release.py
+++ _bld_script/sdk_release.py
@@ -327,9 +327,9 @@ def protectfile(cfg):
     print(cfg)
     bld = get_bld_path(rls_config(cfg[0]['rls_example'][0]))
     print(bld)
-    #cur=get_dir_path(os.path.join(locallocation,'example'),3)
-    #remove_example=find_del_fold(bld,cur)
-    '''
+    cur=get_dir_path(os.path.join(locallocation,'example'),3)
+    remove_example=find_del_fold(bld,cur)
+    
     for del_path in remove_example:
         # print(del_path)
         #f_path = 'aliGenie_bleMesh'
@@ -340,7 +340,7 @@ def protectfile(cfg):
             cmd = 'rd /s /q ' + del_path
             print(cmd)
             validcmd(cmd)
-    '''
+    
     for del_path in cfg[2]['remove_folder']:
         #libsrcpath = 'clonefile'+ '\\' + del_path[1]  #clone file cannot have a space inside
         libsrcpath = locallocation.split('/')[-1] + '\\' + del_path  # clone file cannot have a space inside
